f4d6ab78a621e3d47e7e34017acda89246b8893e
Anorlondo448 <kanaend0605@gmail.com>
2019-12-21 01:05:12 +0900

    add security group for ecs service

--- security_group.tf
+++ security_group.tf
@@ -2,7 +2,6 @@
 #
 # Security Group
 #
-
 resource "aws_security_group" "alb" {
   name        = "${var.app-name}-alb"
   description = "Allow HTTP from Any"
@@ -16,6 +15,29 @@ resource "aws_security_group" "alb" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_service" {
+  name        = "${var.app-name}-ecs-service"
+  description = "Allow HTTP from ALB"
+  vpc_id      = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.app-name}-ecs-service"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups      = [aws_security_group.alb.id]
   }
 
   egress {
