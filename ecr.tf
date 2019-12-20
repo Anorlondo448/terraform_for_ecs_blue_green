9fa6c8f5532b4e33a9d0d34643c2c2853e3645c1
Anorlondo448 <kanaend0605@gmail.com>
2019-12-20 22:31:25 +0900

    add ecr repository

---
+++ ecr.tf
@@ -0,0 +1,12 @@
+###
+#
+# ECR
+#
+resource "aws_ecr_repository" "repository" {
+  name                 = var.app-name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
